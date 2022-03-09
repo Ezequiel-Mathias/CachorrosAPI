546f23d38a2bdc1257e74a02cedb9b181cc2043a
ezequielmathias32@gmail.com <ezequielmathias32@gmail.com>
2022-03-09 16:39:27 -0300

    semi-finalizado

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict'
+
+
+const criarimg = (imagem) =>{
+    const img = document.createElement('img')
+    img.src = imagem
+
+    return img
+}
+
+const pesquisarCachorro = async (raca) =>{
+    const url = `https://dog.ceo/api/breeds/${raca}/image`
+
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    return data
+}
+
+const carregarimagens = async () => {
+    const container = document.getElementById('imagem-conteiner')
+    const raca = document.getElementById('raca').value
+    const imagens = await pesquisarCachorro(raca)
+    const tagimg = imagens.message.map(criarimg)
+    container.replaceChildren(...tagimg)
+}
+
+document.getElementById('pesquisar').addEventListener('click', carregarimagens)
